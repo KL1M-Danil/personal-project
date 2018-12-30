0aa22a66dea3565b5409e747eb0be174e412b33f
Danil <danil@MacBook-Pro-Danil.local>
2018-12-30 20:33:48 +0200

    adds form

---
+++ profile/src/js/scripts.js
@@ -0,0 +1,119 @@
+//АНИМАЦИЯ ЗАЯВКИ
+var current = null;
+document.querySelector("#email").addEventListener("focus", function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: "path",
+    strokeDashoffset: {
+      value: 0,
+      duration: 700,
+      easing: "easeOutQuart"
+    },
+    strokeDasharray: {
+      value: "240 1386",
+      duration: 700,
+      easing: "easeOutQuart"
+    }
+  });
+});
+document.querySelector("#password").addEventListener("focus", function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: "path",
+    strokeDashoffset: {
+      value: -336,
+      duration: 700,
+      easing: "easeOutQuart"
+    },
+    strokeDasharray: {
+      value: "240 1386",
+      duration: 700,
+      easing: "easeOutQuart"
+    }
+  });
+});
+document.querySelector("#submit").addEventListener("focus", function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: "path",
+    strokeDashoffset: {
+      value: -730,
+      duration: 700,
+      easing: "easeOutQuart"
+    },
+    strokeDasharray: {
+      value: "530 1386",
+      duration: 700,
+      easing: "easeOutQuart"
+    }
+  });
+});
+
+//Aнимация
+
+var delay = anime({
+  targets: "#promo-title",
+  opacity: 1,
+  delay: 500,
+  duration: 3000
+});
+
+var delay1 = anime({
+  targets: "#promo-subtitle",
+  opacity: 1,
+  delay: 1500,
+  duration: 3000
+});
+
+var delay2 = anime({
+  targets: "#promo-text",
+  opacity: 1,
+  delay: 2500,
+  duration: 3000
+});
+
+var delay3 = anime({
+  targets: "#review-item",
+  opacity: 1,
+  delay: 3000,
+  duration: 3000
+});
+
+//Заявка
+
+$("body form.form").submit(function() {
+  var th = $(this);
+  $.ajax({
+    type: "POST",
+    url: "mail.php",
+    data: th.serialize()
+  }).done(function() {
+    $("body")
+      .find(".success")
+      .addClass("active")
+      .hide()
+      .fadeIn();
+    setTimeout(function() {
+      $("body")
+        .find(".success")
+        .removeClass("active")
+        .fadeOut();
+      th.trigger("reset");
+    }, 8000);
+  });
+  return false;
+});
+
+// Закрыть попап «спасибо»
+$(".js-close-thank-you").click(function() {
+  // по клику на крестик
+  $(".js-overlay-thank-you").fadeOut();
+});
+
+$(document).mouseup(function(e) {
+  // по клику вне попапа
+  var popup = $(".popup");
+  if (e.target != popup[0] && popup.has(e.target).length === 0) {
+    $(".js-overlay-thank-you").fadeOut();
+  }
+});
